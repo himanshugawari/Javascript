d0d5644f7f4913430d53dfdb127550065c221fbc
Himanshu Gawari <himanshugawarigit@gmail.com>
2020-12-18 09:38:31 +0530

    else if statements

--- 01_Javascript_From_Start/04_else_if_statements.js
+++ 01_Javascript_From_Start/04_else_if_statements.js
@@ -7,8 +7,17 @@ var last = false;
 // falsey values
 // false, null, undefined, 0, "", '', NaN
 
-if (age === 30) {
-  // do something if true
+if (age <= 40) {
+  // if age is less than or equal to 30
+} else if (age > 40 && age < 50) {
+  // if age is greater than 40 and less than 50
+  // && - AND
+  // || - OR
+} else if (age < 0) {
+  // prevent something from happening
 } else {
-  // do something if false
+  // if age greater than 50
 }
+
+// Comparison Operators
+// < <= > >= == === != !== !
