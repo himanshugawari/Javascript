c9382716c85ff9c35c3b0436be8913643b1fa9e4
Himanshu Gawari <himanshugawarigit@gmail.com>
2020-12-18 09:31:33 +0530

    variables and data types

---
+++ 01_Javascript_From_Start/02_Working_with_variables_and_datatypes.js
@@ -0,0 +1,30 @@
+// Decalring & Assigning the variables
+var name = 'Himanshu'; //DataType String
+var id = 234234; //DataType Number
+var alive = true; //DataType Boolean
+var fiveDs; // var default is undefined
+
+name = 'Hardik';
+name = name + ' Gawari'; // String concatination using +
+console.log(name);
+
+id = 7 + 3;
+console.log(id);
+id = id - 2;
+console.log(id);
+id = id * 4;
+console.log(id);
+id = id / 4;
+console.log(id);
+
+fiveDs = ['cat', 'bat', 'mat', 'pat', 'hat']; // Array (list) complex data type
+console.log(fiveDs);
+console.log(fiveDs.length); // length property of array
+console.log(fiveDs[0] + ' ' + fiveDs[3]);
+
+// Complex DataTypes
+
+// Array
+// Object
+// Date
+// DOM Object - Document Object Model
