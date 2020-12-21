4addc699ba2de2d88b192ce510ed3f420ec903a2
Himanshu Gawari <himanshugawarigit@gmail.com>
2020-12-21 13:24:16 +0530

    manipulating and sorting arrays

--- 01_Javascript_From_Start/07_arrays.js
+++ 01_Javascript_From_Start/07_arrays.js
@@ -19,3 +19,36 @@ log(names);
 names.shift();
 names.shift();
 log(names);
+
+// splice(starting index,no. of elements,replaceing elements)
+names.splice(0, 1, 'Rey');
+log(names);
+
+names.splice(1, 2, 'kylo');
+log(names);
+
+names.splice(1, 1, 'C3P0', 'R2D2');
+log(names);
+
+names.splice(1, 2);
+log(names);
+
+names.push('BB8');
+names.push('Obiwan');
+log(names);
+
+log(names);
+names.reverse();
+log(names);
+names.sort();
+log(names);
+
+names.push('Obiwan');
+log(names);
+
+var position = names.indexOf('Luke');
+log(position); //not found return -1
+var position = names.indexOf('Obiwan');
+log(position); // found returns actual index
+var position = names.lastIndexOf('Obiwan');
+log(position); // found returns actual index
