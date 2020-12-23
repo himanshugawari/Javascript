873f0b0fc6267bbff6fd41762ead89532e24bb7c
Himanshu Gawari <himanshugawarigit@gmail.com>
2020-12-23 20:27:49 +0530

    for of loop

--- 01_Javascript_From_Start/39_for_of_loop.js
+++ 01_Javascript_From_Start/39_for_of_loop.js
@@ -12,18 +12,32 @@ let supernatural = {
   seasons: 12,
 };
 
-for (prop in supernatural) {
-  console.log(
-    prop,
-    typeof supernatural[prop],
-    Array.isArray(supernatural[prop])
-  );
+for (props in supernatural) {
+  console.log(props, supernatural[props]);
+}
+console.log();
+
+for (props in supernatural.actors) {
+  console.log(props, supernatural.actors[props]);
+}
+console.log();
+
+for (props of supernatural.actors) {
+  console.log(props);
 }
 
-for (prop in supernatural.actors) {
-  console.log(prop, supernatural.actors[prop]);
-}
+// for (prop in supernatural) {
+//   console.log(
+//     prop,
+//     typeof supernatural[prop],
+//     Array.isArray(supernatural[prop])
+//   );
+// }
 
-for (name of supernatural.actors) {
-  console.log(name);
-}
+// for (prop in supernatural.actors) {
+//   console.log(prop, supernatural.actors[prop]);
+// }
+
+// for (name of supernatural.actors) {
+//   console.log(name);
+// }
